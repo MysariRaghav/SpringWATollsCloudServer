57c548a96a1ce2c5b6d723b6d5e5feab789408e8
raghavmysari <raghavmysari@gmail.com>
2017-06-27 22:49:52 -0400

    Project Intialization

---
+++ src/main/java/com/spring/cloud/cloudserver/CloudServerApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.cloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerApplication.class, args);
+	}
+}
